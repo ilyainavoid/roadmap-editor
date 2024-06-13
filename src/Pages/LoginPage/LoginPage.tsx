878b2575f7de02518d2349e4ad089d0af63ca23c
Vasandr15 <vasandrey007@gmail.com>
2024-06-14 01:59:49 +0700

    refactor: login and registration page not allowed for authorized users

--- src/Pages/LoginPage/LoginPage.tsx
+++ src/Pages/LoginPage/LoginPage.tsx
@@ -1,8 +1,21 @@
-import React from "react";
+import React, {useEffect} from "react";
 import {Flex} from "antd";
 import LoginForm from "../../Components/LoginForm/LoginForm.tsx";
+import {useSelector} from "react-redux";
+import {RootState} from "../../Redux/rootReducer.ts";
+import {useNavigate} from "react-router-dom";
+import {routes} from "../../Consts/routes.ts";
 
 const LoginPage : React.FC = () => {
+    const isAuth = useSelector((state: RootState) => state.auth.isAuth);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (isAuth) {
+            navigate(routes.root());
+        }
+    }, [isAuth, navigate]);
+
     return(
         <>
             <Flex className={"container"}>
--- src/Pages/RegistrationPage/RegistrationPage.tsx
+++ src/Pages/RegistrationPage/RegistrationPage.tsx
@@ -1,8 +1,21 @@
 import {Flex} from "antd";
-import React from "react";
+import React, {useEffect} from "react";
 import RegistrationForm from "../../Components/RegistrationPage/RegistrationForm.tsx";
+import {useSelector} from "react-redux";
+import {RootState} from "../../Redux/rootReducer.ts";
+import {useNavigate} from "react-router-dom";
+import {routes} from "../../Consts/routes.ts";
 
 const RegistrationPage: React.FC = () => {
+    const isAuth = useSelector((state: RootState) => state.auth.isAuth);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (isAuth) {
+            navigate(routes.root());
+        }
+    }, [isAuth, navigate]);
+
     return (
         <>
             <Flex className={"container"}>
