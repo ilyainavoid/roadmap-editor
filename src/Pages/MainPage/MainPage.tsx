879bb80bb5916556be95be6009f063552a462636
Vasandr15 <vasandrey007@gmail.com>
2024-06-26 23:54:18 +0700

    fix: main page edited

--- src/Pages/MainPage/MainPage.tsx
+++ src/Pages/MainPage/MainPage.tsx
@@ -1,34 +1,44 @@
-import {Flex, Typography} from "antd";
+import { Button, Space, Typography } from "antd";
 import React from "react";
 import ListOfRoadmaps from "../../Components/ListOfRadmaps/ListOfRoadmaps.tsx";
-import {useSelector} from "react-redux";
-import {RootState} from "../../Redux/rootReducer.ts";
-import {WELCOME_STRING} from "../../Consts/strings.ts";
+import { useSelector } from "react-redux";
+import { RootState } from "../../Redux/rootReducer.ts";
+import { useNavigate } from "react-router-dom";
+import { routes } from "../../Consts/routes.ts";
+import { WELCOME_STRING } from "../../Consts/strings.ts";
 
-const {Title, Text} = Typography;
+const { Title } = Typography;
 
 const MainPage: React.FC = () => {
     const isAuth = useSelector((state: RootState) => state.auth.isAuth);
+    const navigate = useNavigate();
 
     return (
-        <>
-            <Flex className="container" vertical>
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '80vh' }}>
+            <Space direction="vertical" style={{ textAlign: 'center', width: '100%' }}>
                 {isAuth ? (
                     <>
                         <Title level={1}>Недавно посещенные</Title>
-                        <ListOfRoadmaps/>
+                        <ListOfRoadmaps />
                     </>
+                ) : (
+                    <Space direction="vertical" style={{ textAlign: 'center', width: '100%' }}>
+                        <Title level={1} style={{ fontSize: '24px' }}>
+                            Привет, ты выбрал правильный путь
+                        </Title>
+                        <Title level={1} style={{ fontSize: '24px' }}>
+                            Создавай дорожные карты, делись ими и изучай новое!
+                        </Title>
 
-
-                ) : (<>
-                        <Text>{WELCOME_STRING}</Text>
-                    </>
-
+                        <Space direction="horizontal" style={{ justifyContent: 'center', width: '100%', marginTop: "20px" }}>
+                            <Button type="primary" onClick={() => navigate(routes.registration())}>Зарегистрироваться</Button>
+                            <Button onClick={() => navigate(routes.login())}>Войти</Button>
+                        </Space>
+                    </Space>
                 )}
-            </Flex>
-        </>
-
+            </Space>
+        </div>
     );
 };
 
-export default MainPage
+export default MainPage;
